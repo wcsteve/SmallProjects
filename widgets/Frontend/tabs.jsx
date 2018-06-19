46dd1295ed61199aa7c17a88ef78ce8777f79501
wcsteve <wcsteve9@gmail.com>
2018-06-18 18:09:41 -0700

    Start tabs component

---
+++ widgets/Frontend/tabs.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Tabs extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+
+    return(
+      <div className='tabs'>
+        <h1>Tabs live here</h1>
+      </div>
+    )
+  }
+}
+
+export default Tabs;
--- widgets/root.jsx
+++ widgets/root.jsx
@@ -2,10 +2,26 @@ import React from 'react';
 import Clock from './Frontend/clock';
 import Tabs from './Frontend/tabs';
 
+const TabsContent = [
+  {
+    title: 'one',
+    content: "let's put some happy little content here"
+  },
+  {
+    title: 'two',
+    content: "maybe some soccer scores"
+  },
+  {
+    title: 'three',
+    content: "even more?!?!?!"
+  }
+]
+
 const Root = () => {
   return (
     <div className='app-main'>
       <Clock/>
+      <Tabs/>
     </div>
   )
 }
